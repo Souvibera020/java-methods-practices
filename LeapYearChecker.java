f20cba774bcc9e30c37887d3b310e9b6e167bddb
Souvik Bera <souvikbera020@gmail.com>
2025-03-04 14:05:09 +0530

    [souvik]des:Implemented program to check if a year is a Leap Year based on Gregorian calendar rules.

---
+++ LeapYearChecker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LeapYearChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a year (greater than or equal to 1582):");
+        int year = scanner.nextInt();
+        
+        if (year < 1582) {
+            System.out.println("Year must be greater than or equal to 1582.");
+        } else {
+            if (isLeapYear(year)) {
+                System.out.println(year + " is a Leap Year.");
+            } else {
+                System.out.println(year + " is not a Leap Year.");
+            }
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
