5ab92a0b72ed97844f04ad80c2bda2be8a9a6ceb
Souvik Bera <souvikbera020@gmail.com>
2025-03-04 14:10:52 +0530

    [souvik]des:Extended UnitConverter utility class with new static methods for converting yards-feet, feet-yards, meters-inches, inches-meters, and inches-cm.

---
+++ UnitConverterYards.java
@@ -0,0 +1,59 @@
+public class UnitConverterYards {
+    private static final double KM_TO_MILES = 0.621371;
+    private static final double MILES_TO_KM = 1.60934;
+    private static final double METERS_TO_FEET = 3.28084;
+    private static final double FEET_TO_METERS = 0.3048;
+    private static final double YARDS_TO_FEET = 3.0;
+    private static final double FEET_TO_YARDS = 0.333333;
+    private static final double METERS_TO_INCHES = 39.3701;
+    private static final double INCHES_TO_METERS = 0.0254;
+    private static final double INCHES_TO_CM = 2.54;
+
+    public static double convertKmToMiles(double km) {
+        return km * KM_TO_MILES;
+    }
+
+    public static double convertMilesToKm(double miles) {
+        return miles * MILES_TO_KM;
+    }
+
+    public static double convertMetersToFeet(double meters) {
+        return meters * METERS_TO_FEET;
+    }
+
+    public static double convertFeetToMeters(double feet) {
+        return feet * FEET_TO_METERS;
+    }
+
+    public static double convertYardsToFeet(double yards) {
+        return yards * YARDS_TO_FEET;
+    }
+
+    public static double convertFeetToYards(double feet) {
+        return feet * FEET_TO_YARDS;
+    }
+
+    public static double convertMetersToInches(double meters) {
+        return meters * METERS_TO_INCHES;
+    }
+
+    public static double convertInchesToMeters(double inches) {
+        return inches * INCHES_TO_METERS;
+    }
+
+    public static double convertInchesToCm(double inches) {
+        return inches * INCHES_TO_CM;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("10 km to miles: " + convertKmToMiles(10));
+        System.out.println("10 miles to km: " + convertMilesToKm(10));
+        System.out.println("10 meters to feet: " + convertMetersToFeet(10));
+        System.out.println("10 feet to meters: " + convertFeetToMeters(10));
+        System.out.println("10 yards to feet: " + convertYardsToFeet(10));
+        System.out.println("10 feet to yards: " + convertFeetToYards(10));
+        System.out.println("10 meters to inches: " + convertMetersToInches(10));
+        System.out.println("10 inches to meters: " + convertInchesToMeters(10));
+        System.out.println("10 inches to cm: " + convertInchesToCm(10));
+    }
+}
